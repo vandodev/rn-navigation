69da47b0a661000b19f0fc187c6c3f395ff9e77d
vandodev <vandoaparecido@hotmail.com>
2025-05-11 07:14:45 -0300

    :arrow_heading_down: :arrow_right_hook: :leftwards_arrow_with_hook: Navegando com useNavigation

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,14 +1,22 @@
+import { View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 import { ButtonIcon } from '@/components/ButtonIcon'
+
 import { Header } from '@/components/Header'
 import { Title } from '@/components/Title'
-import { View } from 'react-native'
 
 export function Home() {
+  const navigation = useNavigation()
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
-        <Title>Home</Title>
-        <ButtonIcon name="add-circle" />
+        <Title>Home</Title>        
+        
+        <ButtonIcon
+          name="add-circle"
+          onPress={() => navigation.navigate('product')}
+        />
+
       </Header>
     </View>
   )
--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -1,13 +1,21 @@
+import { View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
 import { ButtonIcon } from '@/components/ButtonIcon'
 import { Header } from '@/components/Header'
 import { Title } from '@/components/Title'
-import { View } from 'react-native'
 
 export function Product() {
+  const navigation = useNavigation()
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
-        <ButtonIcon name="arrow-circle-left" />
+
+        <ButtonIcon
+          name="arrow-circle-left"
+          onPress={() => navigation.goBack()}
+        />
+
         <Title>Product</Title>
       </Header>
     </View>
