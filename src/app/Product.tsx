59f6a4754a1729c922b6dfb4f90d5c0a9ef3710a
vandodev <vandoaparecido@hotmail.com>
2025-05-12 08:20:58 -0300

    :arrow_heading_down: :arrow_right_hook: :leftwards_arrow_with_hook: Voltando ao estado anterior, buscando parâmetro com route

--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -1,15 +1,13 @@
 import { View } from 'react-native'
-import { useRoute } from '@react-navigation/native'
 
 import { ButtonIcon } from '@/components/ButtonIcon'
 import { Header } from '@/components/Header'
 import { Title } from '@/components/Title'
 import { StackRoutesProps } from '@/routes/StackRoutes'
 
-type routeParams = StackRoutesProps<"product">
 
 export function Product({ navigation, route }: StackRoutesProps<'product'>) {
-  const {params} = useRoute<routeParams["route"]>()
+  
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
@@ -20,7 +18,7 @@ export function Product({ navigation, route }: StackRoutesProps<'product'>) {
         />
 
         {/*<Title>Product {route.params?.id}</Title> */}
-        <Title>Product {params?.id}</Title>
+        <Title>Product {route.params?.id}</Title>
       </Header>
     </View>
   )
