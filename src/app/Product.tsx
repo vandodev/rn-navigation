8cd5a802adb78e709ddab7e5bdd3d2a0e2396be6
vandodev <vandoaparecido@hotmail.com>
2025-05-12 08:11:09 -0300

    :arrow_heading_down: :arrow_right_hook: :leftwards_arrow_hook: Pegando parâmetro com useRoute

--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -1,11 +1,15 @@
 import { View } from 'react-native'
+import { useRoute } from '@react-navigation/native'
 
 import { ButtonIcon } from '@/components/ButtonIcon'
 import { Header } from '@/components/Header'
 import { Title } from '@/components/Title'
 import { StackRoutesProps } from '@/routes/StackRoutes'
 
+type routeParams = StackRoutesProps<"product">
+
 export function Product({ navigation, route }: StackRoutesProps<'product'>) {
+  const {params} = useRoute<routeParams["route"]>()
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
@@ -15,7 +19,8 @@ export function Product({ navigation, route }: StackRoutesProps<'product'>) {
           onPress={() => navigation.goBack()}
         />
 
-        <Title>Product {route.params?.id}</Title>
+        {/*<Title>Product {route.params?.id}</Title> */}
+        <Title>Product {params?.id}</Title>
       </Header>
     </View>
   )
