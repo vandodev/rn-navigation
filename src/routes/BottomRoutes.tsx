928f986fff1df255a0df89cc07a49fd3e7d6e371
vandodev <vandoaparecido@hotmail.com>
2025-05-13 07:35:29 -0300

    :arrow_heading_down: :arrow_right_hook: :leftwards_arrow_hook: Customizando Bottom Navigator

--- src/routes/BottomRoutes.tsx
+++ src/routes/BottomRoutes.tsx
@@ -3,6 +3,8 @@ import {
   BottomTabScreenProps,
 } from '@react-navigation/bottom-tabs'
 
+import { MaterialIcons } from '@expo/vector-icons'
+
 import { Home } from '@/app/Home'
 import { Product } from '@/app/Product'
 
@@ -20,10 +22,33 @@ export function BottomRoutes() {
   return (
     <Tab.Navigator
       initialRouteName="home"
-      screenOptions={{ headerShown: false }}
+       screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#2C46B1',
+        tabBarInactiveTintColor: '#444444',
+        tabBarLabelPosition: 'beside-icon',
+      }}
     >
-      <Tab.Screen name="home" component={Home} />
-      <Tab.Screen name="product" component={Product} />
+ <Tab.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Ínicio',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="product"
+        component={Product}
+        options={{
+          tabBarLabel: 'Produto',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="add-circle" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   )
 }
