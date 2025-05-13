a0a247894c2cecaeeb5d2bad1881717c80433d07
vandodev <vandoaparecido@hotmail.com>
2025-05-13 08:20:10 -0300

    :arrow_heading_down: :arrow_right_hook: :leftwards_arrow_hook: toggleDrawer

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -10,12 +10,14 @@ export function Home({ navigation }: DrawerRoutesProps<'home'>) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
-        <Title>Home</Title>        
-        
+               
         <ButtonIcon
           name="add-circle"
           onPress={() => navigation.navigate('product', { id: '153' })}
         />
+
+        <Title>Home</Title>
+        <ButtonIcon name="menu" onPress={() => navigation.toggleDrawer()} />
 
       </Header>
     </View>
